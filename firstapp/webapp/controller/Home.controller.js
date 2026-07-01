2602fc45d4ebe34a5fb138132508d03fad07c187
omatheussantos <matheus.ocs@gmail.com>
2026-06-30 22:41:09 -0300

    Add MessageToast Controller Onpress buttons

--- firstapp/webapp/controller/Home.controller.js
+++ firstapp/webapp/controller/Home.controller.js
@@ -1,10 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageToast) {
+        "use strict";
 
-    return Controller.extend("com.lab2dev.firstapp.controller.Home", {
-        onInit() {
-        }
-    });
+        return Controller.extend("com.lab2dev.firstapp.controller.Home", {
+            onInit: function() {
+            },
+
+            onPress: function (oEvent) {
+
+                // Origem do evento
+                const item = oEvent.getSource();
+
+                // Título do item
+                const itemTitle = item.getTitle();
+
+                // Mensagem a ser exibida
+                const message = `O item "${itemTitle}" foi clicado!`;
+
+                // Exibe uma mensagem na tela
+                MessageToast.show(message);
+            }
+        });
 });
