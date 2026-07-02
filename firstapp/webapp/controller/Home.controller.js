e4ea84bbe817204d2cab641aab21deee69f82b63
omatheussantos <matheus.ocs@gmail.com>
2026-07-02 08:54:31 -0300

    Add JSONModel and Aggregation Binding to make list dynamic

--- firstapp/webapp/controller/Home.controller.js
+++ firstapp/webapp/controller/Home.controller.js
@@ -1,16 +1,35 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast, MessageBox) {
+    function (Controller, MessageBox, JSONModel) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Home", {
             onInit: function() {
+                // Simulando uma lista de produtos
+                const products = [
+                    {title: "Arroz", counter: 5},
+                    {title: "Feijão", counter: 8},
+                    {title: "Macarrão", counter: 10},
+                    {title: "Óleo de Cozinha", counter: 3},
+                    {title: "Leite", counter: 7},
+                    {title: "Café", counter: 4},
+                    {title: "Açúcar", counter: 6},
+                    {title: "Sal", counter: 2},
+                    {title: "Farinha de Trigo", counter: 9},
+                    {title: "Sabonete", counter: 11},
+                ];
+
+                // Cria um modelo JSON com os produtos (JSONModel)
+                const oModel = new JSONModel(products);
+
+                // Define o modelo com o nome "products" e fazendo a ligação com a view (Aggregation Binding)
+                this.getView().setModel(oModel, "products");
             },
 
             onPress: function (oEvent) {
@@ -23,9 +42,6 @@ sap.ui.define([
 
                 // Mensagem a ser exibida
                 const message = `O item "${itemTitle}" foi clicado!`;
-
-                // Exibe uma mensagem na tela
-                //MessageToast.show(message);
                 
 			    MessageBox.information(message, {
                     title: "Informação do item"
