f574b63ea2691dcaae0850091e2f3cc141c31aa7
omatheussantos <matheus.ocs@gmail.com>
2026-07-12 19:27:22 -0300

    migrate models to models.js

--- firstapp/webapp/controller/Home.controller.js
+++ firstapp/webapp/controller/Home.controller.js
@@ -1,34 +1,30 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/odata/v2/ODataModel"
+    "com/lab2dev/firstapp/model/models"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox, JSONModel, ODataModel) {
+    function (Controller, MessageBox, models) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Home", {
             onInit: function() {
-                const oDataModel = new ODataModel("/northwind/northwind.svc/");
-
-                oDataModel.read("/Products", {
-                    success: (oProducts) => {
-                        console.log(oProducts);
-                        const products = oProducts.results;
-
-                        console.log(products);
-
-                        const oModel = new JSONModel(products);
-                        this.getView().setModel(oModel, "products");
-                    },
-                    error: (oError) => {
-                        MessageBox.error("Erro ao carregar os dados");
+                const params = {
+                    urlParameters: {
+                        $expand: "Category"
                     }
+                };
+
+                const products = models.getProducts(params);
+
+                products.then((oProductsModel) => {
+                    this.getView().setModel(oProductsModel, "products");
+                }).catch((oError) => {
+                    MessageBox.error(oError);
                 });
-            },
+        },
 
             onPress: function (oEvent) {
 
@@ -44,7 +40,6 @@ sap.ui.define([
 			    MessageBox.information(message, {
                     title: "Informação do item"
                 });
-
             }
-        });
+    });
 });
--- firstapp/webapp/model/models.js
+++ firstapp/webapp/model/models.js
@@ -1,19 +1,57 @@
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel",
     "sap/ui/Device"
 ], 
-function (JSONModel, Device) {
+    /**
+     * provide app-view type models (as in the first "V" in MVVC)
+     * 
+     * @param {typeof sap.ui.mode.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.Device} Device
+     * 
+     * @returns {Function} createDeviceModel() for providing runtime info for the device the UI5 app is running on
+     */
+
+
+function (JSONModel, ODataModel, Device) {
     "use strict";
 
     return {
-        /**
-         * Provides runtime information for the device the UI5 app is running on as a JSONModel.
-         * @returns {sap.ui.model.json.JSONModel} The device model.
-         */
         createDeviceModel: function () {
             var oModel = new JSONModel(Device);
             oModel.setDefaultBindingMode("OneWay");
             return oModel;
+        },
+
+        getODataModel: function () {
+            var oDataModel = new ODataModel("/northwind/northwind.svc/");
+
+            return new Promise(function (resolve, reject) {
+                oDataModel.attachMetadataLoaded(() => {
+                    resolve(oDataModel);
+                });
+                oDataModel.attachMetadataFailed(() => {
+                    reject("Serviço indisponível no momento.");
+                });
+            });
+        },
+
+        getProducts: function (oURLParam) {
+            var oDataModel = this.getODataModel();
+
+            return new Promise((resolve, reject) => {
+                oDataModel.then((oModel) => {
+                    oModel.read("/Products", {
+                        ...oURLParam,
+                        success: (oData) => {
+                            resolve(new JSONModel(oData.results));
+                        },
+                        error: (oError) => {
+                            reject(oError);
+                        }
+                    });
+                });
+            });
         }
     };
 
